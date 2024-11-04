9f5c8cf912b39a02e899e925acbc2da3cb907279
Thomas Glasser <thomas@thomasglasser.dev>
2024-11-03 21:32:52 -0500

    Initial commit

---
+++ fabric/src/main/java/dev/thomasglasser/multiloadertemplate/MultiloaderTemplateFabric.java
@@ -0,0 +1,10 @@
+package dev.thomasglasser.multiloadertemplate;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MultiloaderTemplateFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        MultiloaderTemplate.init();
+    }
+}
